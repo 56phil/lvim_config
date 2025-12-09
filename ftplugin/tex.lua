939a75b3f515f8f98b9c3333518d673dd5465853
Phil Huffman <philhuffman56@icloud.com>
2025-12-09 12:57:27 -0600

    update keymap

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -1,3 +1,4 @@
 -- Better formatting for TeX files
+
 vim.opt_local.formatoptions:append({ "a", "w" })
 vim.opt_local.formatoptions:remove({ "t" })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Write all buffers
+vim.keymap.set("n", "<leader>bA", ":wall<CR>", {
+  desc = "Write all buffers to DASD",
+  silent = true,
+})
+
 -- Buffer write
 vim.keymap.set("n", "<leader>bw", ":write<CR>", {
   desc = "Write current buffer to DASD",
@@ -17,7 +23,5 @@ vim.keymap.set("n", "<leader>bs", ":set spell!<CR>", {
 vim.keymap.set("n", "]s", "]s", { desc = "Next Spelling Error" })
 vim.keymap.set("n", "[s", "[s", { desc = "Previous Spelling Error" })
 vim.keymap.set("n", "z=", "z=", { desc = "Suggest Correction" })
--- vim.keymap.set("n", "<leader>bw", ":write<CR>", { desc = "Write current buffer to DASD" })
--- vim.keymap.set("n", "<leader>bs", ":set spell!<CR>", { desc = "Toggle Spell Check" })
 vim.keymap.set("n", "<leader>bc", ":RemoveContractions<CR>", { desc = "Remove contractions" })
 vim.keymap.set("n", "<leader>ba", ":RemoveContractions all<CR>", { desc = "Remove contractions from all buffers" })
