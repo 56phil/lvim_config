b1ac9e16e50057f54bfcc091372ac423881cab63
Philip Huffman <philip.r.huffman@gmail.com>
2026-03-03 18:25:13 -0600

    ,odify .gitignore autocmds.lua keymaps.lua options.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>bw", ":write<CR>", { desc = "Write current buffer to DASD" })
+vim.keymap.set("n", "<leader>bW", ":wall<CR>", { desc = "Write all buffers to DASD" })
+vim.keymap.set("n", "<leader>bs", ":set spell!<CR>", { desc = "Toggle Spell Check" })
+vim.keymap.set("n", "<leader>bc", ":RemoveContractions<CR>", { desc = "Remove contractions" })
+vim.keymap.set("n", "<leader>ba", ":RemoveContractions all<CR>", { desc = "Remove contractions from all buffers" })
+vim.keymap.set("n", "]s", "]s", { desc = "Next Spelling Error" })
+vim.keymap.set("n", "[s", "[s", { desc = "Previous Spelling Error" })
+vim.keymap.set("n", "z=", "z=", { desc = "Suggest Correction" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.colorcolumn = "73" -- column 73 marks overflow past column 72
+vim.cmd([[highlight ColorColumn guibg=#3c3836]]) -- a muted dark gray for example
+vim.opt.spellfile = vim.fn.expand("~/.config/nvim/spell/en.utf-8.add")
